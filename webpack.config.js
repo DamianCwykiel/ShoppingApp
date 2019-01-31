82eb5eb2b6fde9b4c84cdb201b1a7762b6caeef3
DamianCwykiel <damian.cwykiel@gmail.com>
2019-01-31 13:53:03 +0100

    setup production build & server

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 //entry->output/
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-//console.log(path.join(__dirname, 'public'));
-
-module.exports = {
-    entry: './src/app.js', //we tell webpack where it should start//
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test:/\.s?css$/,//? = and (css & scss)
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map', //tracks and shows our bugs position
-    devServer:{
-        contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+    const isProduction = env ==='production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    
+    console.log('env', env);
+    return {
+        entry: './src/app.js', //we tell webpack where it should start//
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test:/\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                          loader: 'css-loader',
+                          options: {
+                              sourceMap: true
+                          }  
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMAp: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map', //tracks and shows our bugs position
+        devServer:{
+            contentBase: path.join(__dirname, 'public')
+        }
     }
 };
+
+//console.log(path.join(__dirname, 'public'));
