155675e9c10acbd2859957ff17847cc9d497a87c
Damian Cwykiel <damian.cwykiel@gmail.com>
2019-07-13 17:44:56 +0200

    addOptions l-24 autocompleted added

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -21,7 +21,7 @@ export default class AddOption extends React.Component{
         <div>
           {this.state.error && <p className="add-option-error">{this.state.error}</p>}
           <form className ="add-option" onSubmit={this.handleAddOption}>
-            <input className="add-option__input" type="text" name="option"/>
+            <input className="add-option__input" type="text" name="option" autocomplete="off"/>
             <button className="button">Add Item</button>
           </form>
         </div>
